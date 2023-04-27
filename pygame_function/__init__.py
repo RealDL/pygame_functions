257d44bc1f7099ee6583b4d815b75f1ba6fc907b
DL <91336924+TheRealDL1@users.noreply.github.com>
2023-04-27 17:17:55 +0100

    Update __init__.py

--- pygame_function/__init__.py
+++ pygame_function/__init__.py
@@ -1,1 +1,6 @@
-
+from pygame_function.functions import Button
+from pygame_function.functions import Text
+from pygame_function.functions import TextBox
+from pygame_function.functions import Animation
+from pygame_function.functions import WordButton
+from pygame_function.functions import Images
