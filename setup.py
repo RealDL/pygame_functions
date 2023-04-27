84a73380fd62e190bd813be5d5d6070b4b240b09
DL <91336924+TheRealDL1@users.noreply.github.com>
2023-04-27 17:04:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
 
+VERSION = '0.0.1'
+DESCRIPTION = 'A simple package for pygame.'
+LONG_DESCRIPTION = 'A package that allows to make buttons and textboxes in pygame.'
+
+# Setting up
+setup(
+    name="pygame_functions",
+    version=VERSION,
+    author="DL#6569",
+    author_email="<dldavejackson@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python', 'pygame', 'buttons', 'textboxes'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
